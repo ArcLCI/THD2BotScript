a919f18eb75991f9761d7e93a026fad9d30a9f37
NicholasFRG <198285@oyyeel.com>
2025-07-23 23:49:46 +0800

    女苑换夺舍

---
+++ ability_item_usage_meepo.lua
@@ -0,0 +1,239 @@
+
+require(GetScriptDirectory() ..  "/thd2_item_usage")
+
+----------------------------------------------------------------------------------------------------
+
+local cast02Desire, cast03Desire, cast04Desire = 0, 0, 0
+local ability02, ability03, ability04
+
+local tmp = 0
+
+function MyItemUsageThink()
+
+	local npcBot = GetBot()
+
+	-- Check if we're already using an ability
+	if ( npcBot:IsMuted() or npcBot:IsUsingAbility() ) then return end
+
+	local item_jump = IsItemAvailable( "item_wanmeitiaoyuezhuangzhi" )
+	local item_speed = IsItemAvailable( "item_mystia_wings" ) or
+					IsItemAvailable( "item_brother_sharp" ) or
+					IsItemAvailable( "item_bone_flute" )
+
+	local item_stun = IsItemAvailable( "item_yuetufensuijvren" )
+	if item_stun == nil then
+		item_stun = IsItemAvailable( "item_pocket_watch" )
+	end
+
+	if ( item_jump~=nil and item_jump:IsFullyCastable() )
+	then
+		local castItemJumpDesire, castItemJumpTarget = ConsiderItemJump( item_jump )
+		if ( castItemJumpDesire > 0 )
+		then
+			npcBot:Action_UseAbilityOnLocation( item_jump, castItemJumpTarget)
+			return
+		end
+	end
+
+	if ( item_stun~=nil and item_stun:IsFullyCastable() )
+	then
+		--print("stun item exist")
+		local castItemStunDesire, castItemStunTarget = ConsiderItemStun(item_stun)
+		if ( castItemStunDesire > 0 )
+		then
+			--print("stun luanch")
+			npcBot:Action_UseAbilityOnEntity( item_stun, castItemStunTarget )
+			return
+		end
+	end
+
+	if ( item_speed~=nil and item_speed:IsFullyCastable() )
+	then
+		local castItemSpeedDesire = ConsiderItemSpeed( item_speed )
+		if ( castItemSpeedDesire > 0 )
+		then
+			npcBot:Action_UseAbility( item_speed )
+			return
+		end
+	end
+end
+
+----------------------------------------------------------------------------------------------------
+
+function AbilityUsageThink()
+
+	if not IsBotAwake() then return end
+
+	MyItemUsageThink()
+	ConsiderNeutralItems()
+
+
+	local npcBot = GetBot()
+
+	-- Check if we're already using an ability
+	if ( npcBot:IsSilenced() or npcBot:IsUsingAbility() ) then return end
+
+	ability02 = npcBot:GetAbilityByName( "ability_thdots_Jyoon_2" )
+	ability03 = npcBot:GetAbilityByName( "ability_thdots_Jyoon_3" )
+	ability04 = npcBot:GetAbilityByName( "ability_thdots_Jyoon_4" )
+
+	-- Consider using each ability
+	cast02Desire, cast02Location = ConsiderAbilityJyoon02()
+	if ( cast02Desire > 0 )
+	then
+		npcBot:Action_UseAbilityOnLocation( ability02, cast02Location )
+		return
+	end
+
+	cast03Desire = ConsiderAbilityJyoon03()
+	if ( cast03Desire > 0 )
+	then
+		npcBot:Action_UseAbility( ability03)
+		return
+	end
+
+	cast04Desire = ConsiderAbilityJyoon04()
+
+	if ( cast04Desire > 0 )
+	then
+		npcBot:Action_UseAbility( ability04)
+		return
+	end
+
+end
+
+----------------------------------------------------------------------------------------------------
+
+function CanCastJyoon02OnTarget( npcTarget )
+	return npcTarget:CanBeSeen() and npcTarget:IsHero() and not npcTarget:IsMagicImmune() and not npcTarget:IsInvulnerable()
+end
+
+
+function CanCastJyoon04OnTarget( npcTarget )
+	return npcTarget:CanBeSeen() and npcTarget:IsHero() and not npcTarget:IsMagicImmune() and not npcTarget:IsInvulnerable()
+end
+----------------------------------------------------------------------------------------------------
+
+
+function ConsiderAbilityJyoon02()
+
+	local npcBot = GetBot()
+
+	-- Make sure it's castable
+	if ( not ability02:IsFullyCastable() )
+	then
+		return BOT_ACTION_DESIRE_NONE,0
+	end
+
+	local nCastRange = 700
+	local nRadius = 350 + 25 * ability02:GetLevel()
+
+
+	if ((npcBot:GetActiveMode() == BOT_MODE_ATTACK
+		or npcBot:GetActiveMode() == BOT_MODE_GANK
+		or npcBot:GetActiveMode() == BOT_MODE_RETREAT )
+		and npcBot:GetActiveModeDesire() >= BOT_MODE_DESIRE_HIGH ) then
+		local locationAoE = CachedFindAoELocation( npcBot, 60001, true, true, npcBot:GetLocation(), nCastRange, nRadius, 0, 0 )
+		if locationAoE.count > 1 then
+			return BOT_ACTION_DESIRE_HIGH, locationAoE.targetloc
+		end
+	end
+
+	local tableNearbyEnemyHeroes = CachedGetNearbyHeroes( npcBot, nCastRange + nRadius, true, BOT_MODE_NONE )
+	for _,npcEnemy in pairs( tableNearbyEnemyHeroes )
+	do
+		if ( npcBot:GetTarget() == npcEnemy and CanCastJyoon02OnTarget( npcEnemy )  )
+		then
+				return BOT_ACTION_DESIRE_HIGH, npcEnemy:GetLocation()
+		end
+
+		if ( npcBot:WasRecentlyDamagedByHero( npcEnemy, 2.0 ) )
+		then
+			if ( CanCastJyoon02OnTarget( npcEnemy ) and not IsPossibleIllusion( npcEnemy ))
+			then
+				return BOT_ACTION_DESIRE_MODERATE, npcEnemy:GetLocation()
+			end
+		end
+	end
+
+	-- If we're going after someone
+	if ( npcBot:GetActiveMode() == BOT_MODE_ROAM or
+		 npcBot:GetActiveMode() == BOT_MODE_TEAM_ROAM or
+		 npcBot:GetActiveMode() == BOT_MODE_GANK or
+		 npcBot:GetActiveMode() == BOT_MODE_DEFEND_ALLY )
+	then
+		local npcTarget = npcBot:GetTarget()
+
+		if ( npcTarget ~= nil )
+		then
+			if ( CanCastJyoon02OnTarget( npcTarget ) )
+			then
+				return BOT_ACTION_DESIRE_HIGH, npcTarget:GetLocation()
+			end
+		end
+	end
+
+	return BOT_ACTION_DESIRE_NONE,0
+end
+
+
+----------------------------------------------------------------------------------------------------
+
+function ConsiderAbilityJyoon03()
+
+	local npcBot = GetBot()
+
+	-- Make sure it's castable
+	if ( not ability03:IsFullyCastable() )
+	then
+		return BOT_ACTION_DESIRE_NONE
+	end
+
+	local nRadius = 750
+
+	-- Fighting or Retreating with hero
+	if ( npcBot:GetActiveMode() == BOT_MODE_RETREAT or npcBot:GetActiveMode() == BOT_MODE_ATTACK )
+	then
+		local tableNearbyEnemyHeroes = npcBot:GetNearbyHeroes(nRadius, true, BOT_MODE_NONE )
+		for _,npcEnemy in pairs( tableNearbyEnemyHeroes )
+		do
+			if ( npcEnemy ~= nil and not IsPossibleIllusion( npcEnemy ))
+			then
+				return BOT_ACTION_DESIRE_MODERATE
+			end
+		end
+	end
+	return BOT_ACTION_DESIRE_NONE
+
+end
+
+----------------------------------------------------------------------------------------------------
+
+function ConsiderAbilityJyoon04()
+
+	local npcBot = GetBot()
+
+	-- Make sure it's castable
+	if ( not ability04:IsFullyCastable() )
+	then
+		return BOT_ACTION_DESIRE_NONE
+	end
+
+	local nCastRange = 200
+	if (( npcBot:GetActiveMode() == BOT_MODE_ATTACK or npcBot:GetActiveMode() == BOT_MODE_GANK) and npcBot:GetActiveModeDesire() >= BOT_MODE_DESIRE_MODERATE )
+	then
+		local tableNearbyEnemyHeroes = npcBot:GetNearbyHeroes( nCastRange , true, BOT_MODE_NONE )
+		if #tableNearbyEnemyHeroes > 1 then
+			return BOT_ACTION_DESIRE_HIGH
+		else
+			for _,npcEnemy in pairs( tableNearbyEnemyHeroes )
+			do
+				if (GetCapability(npcEnemy) > GetCapability(npcBot) or npcBot:GetHealth() < npcEnemy:GetHealth()) and not npcEnemy:IsAttackImmune() then
+					return BOT_ACTION_DESIRE_HIGH
+				end
+			end
+		end
+	end
+	return BOT_ACTION_DESIRE_NONE
+end
+
---
+++ item_purchase_meepo.lua
@@ -0,0 +1,76 @@
+
+require(GetScriptDirectory() ..  "/thd2_item_purchase")
+
+
+local tableItemsToBuy = {
+				"item_broom",
+
+				"item_mushroom",
+				"item_cherry_branch",
+					"item_recipe_mushroom_kebab",
+				"item_wind_lace",
+					"item_recipe_9ball",
+				"item_sailor_suit",
+					"item_recipe_mystia_wings",
+
+				"item_god_hand",
+				"item_god_hand",
+					"item_recipe_loneliness",
+
+				"item_bra",
+				"item_baozi",
+				"item_glue",
+					"item_recipe_pad",
+				"item_hammer",
+				"item_throwing_knive",
+				"item_cherry_leaf",
+					"item_recipe_smash_stick",
+					"item_recipe_yuetufensuijvren",
+
+				"item_knife",
+				"item_scissors",
+				"item_recipe_quant",
+				"item_wrench",
+					"item_recipe_sampan",
+
+				"item_mushroom",
+				"item_mushroom",
+					"item_recipe_touhou_banana",
+				"item_cat_foot",
+					"item_recipe_brother_sharp",
+				"item_ice_block",
+					"item_recipe_wanmeitiaoyuezhuangzhi",
+
+				"item_cat_foot",
+				"item_paper_mask",
+				"item_rocket_diagram",
+				"item_zun_hat",
+					"item_recipe_wanbaochui",
+					"item_recipe_wanbaochui2",
+
+				"item_frog",
+				"item_ice_block",
+				"item_ice_block",
+				"item_recipe_frozen",
+			}
+
+
+----------------------------------------------------------------------------------------------------
+
+local seed_id = nil
+local next_purchase = -1
+
+function ItemPurchaseThink()
+	if seed_id == nil then
+		seed_id = RandomInt(1,999999999)
+	end
+	local npcBot = GetBot()
+	local randIndex = RandomInt(1,3)
+	local tableEdible = {"item_mushroom_pie_immediate","item_mushroom_kebab_immediate","item_mushroom_soup_immediate"}
+	if DotaTime() > 0 and next_purchase > 0 and #tableItemsToBuy < next_purchase then
+		table.insert(tableItemsToBuy,tableEdible[2])
+	end
+	next_purchase = ConsiderItemPurchase(tableItemsToBuy,seed_id)
+end
+
+----------------------------------------------------------------------------------------------------
